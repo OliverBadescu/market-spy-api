69eac3d4e7cf7a835c977cf3681809c237d69a33
Badescu Oliver <olibadescu@gmail.com>
2025-03-14 23:02:11 +0200

    added base files

---
+++ src/main/java/app/MarketSpy/MarketSpyApplication.java
@@ -0,0 +1,13 @@
+package app.MarketSpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSpyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/MarketSpy/MarketSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.MarketSpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
